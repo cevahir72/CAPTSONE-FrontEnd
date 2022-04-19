a93ea42098543aa73f3b2d7a224158a84e0eb154
cevahir72 <kelesmevlut71@gmail.com>
2022-04-19 14:21:08 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import { loadUser } from './redux/actions/userAction';
+import AppRouter from './router/AppRouter';
 
 function App() {
+  const dispatch = useDispatch();
+  const token = JSON.parse(localStorage.getItem('token'));
+
+  useEffect(() => {
+    if (token) {
+      dispatch(loadUser(token));
+    }
+  }, [dispatch, token]);
+
   return (
-    <div className="App">
-      <Navbar/>
-     
+    <div>
+      <AppRouter />
+      <ToastContainer />
     </div>
   );
 }
